d50bca9f428f70a7c51e464ed81a4ffc43384209
TarasSydorovych <jakzadarom2@gmail.com>
2022-09-27 14:33:01 +0300

    ÐсÐайт на реакт

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+export default function header(){
+    return(
+        <header>
+            <div>
+                <span className='logo'>Авто коврики</span>
+            </div>
+            <div className='baner'>
+
+            </div>
+        </header>
+    )
+
+}
